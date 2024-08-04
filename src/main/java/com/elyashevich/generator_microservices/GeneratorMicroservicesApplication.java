1b7a5ab72dbcbb1de2d05f320d7929c8893607a7
Artem-Elyashevich <elyashevich.artem06@gmail.com>
2024-08-04 15:49:21 +0300

    First commit

---
+++ src/main/java/com/elyashevich/generator_microservices/GeneratorMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.elyashevich.generator_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratorMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/elyashevich/generator_microservices/model/Data.java
@@ -0,0 +1,4 @@
+package com.elyashevich.generator_microservices.model;
+
+public class Data {
+}
